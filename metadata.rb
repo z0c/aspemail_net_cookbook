8f288a70205910e481c259f05b7c5a82ceaae043
Nuno Pereira <nvnivs@gmail.com>
2015-10-20 15:54:41 +0100

    removed constraint to windows cookbook

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures aspemail_net_cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version          '0.2.0'
+version          '0.2.1'
 
 supports         'windows', '~> 6.2'
-
-depends          'windows', '1.38.2'
+depends 'windows', '~> 1.38.2'
